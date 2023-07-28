3d2cdae7152c1d43475d9e1bee21aad96e25fef2
İbrahim Uslu <ibrahimuslu.git@gmail.com>
2023-07-28 14:28:47 +0300

    Cart Operations such as Display, Add, Delete

---
+++ client/src/CartSummary.js
@@ -0,0 +1,59 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Badge,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
+
+export default class CartSummary extends Component {
+  renderSummary() {
+    return (
+      <UncontrolledDropdown>
+        <DropdownToggle caret>
+          Your Cart - {this.props.cart.length}
+        </DropdownToggle>
+        <DropdownMenu>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              <Badge
+                onClick={() => this.props.removeFromCart(cartItem.product)}
+                color="danger"
+              >
+                X
+              </Badge>
+              {cartItem.product.productName}
+              <Badge color="success">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+          <DropdownItem divider />
+          <DropdownItem>
+            <Link to="cart">Go to Cart</Link>
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+
+  renderEmptyCart() {
+    return (
+      <UncontrolledDropdown>
+        <DropdownToggle>Empty Cart</DropdownToggle>
+      </UncontrolledDropdown>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.cart.length > 0
+          ? this.renderSummary()
+          : this.renderEmptyCart()}
+      </div>
+    );
+  }
+}
